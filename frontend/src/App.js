4c561d1578184a486648bca96f499b5bc6e02a69
adurodo1 <adurodo1@my.centennialcollege.ca>
2022-03-06 02:57:20 -0500

    Search functionality added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,34 +1,64 @@
 import logo from './logo.svg';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Nav from 'react-bootstrap/Nav'
+import {Nav,Navbar,NavDropdown,Container,Button,Form,FormControl} from 'react-bootstrap'
+ import {SearchForm} from './Components/SearchForm'
 
 import { CoinList } from './Components/CoinList';
+import { useState } from 'react';
 
 function App() {
+
+  const[searchformvalue,setSearchformvalue]=useState("")
+
+ 
+  function inputCoin(e){
+
+    console.log( e.target.value)
+  
+
+    setSearchformvalue(e.target.value)
+    
+
+      
+  }
+
   return (
     <>
-<Nav
-  activeKey="/home"
-  onSelect={(selectedKey) => alert(`selected ${selectedKey}`)}
->
-  <Nav.Item>
-    <Nav.Link href="/home">Active</Nav.Link>
-  </Nav.Item>
-  <Nav.Item>
-    <Nav.Link eventKey="link-1">Link</Nav.Link>
-  </Nav.Item>
-  <Nav.Item>
-    <Nav.Link eventKey="link-2">Link</Nav.Link>
-  </Nav.Item>
-  <Nav.Item>
-    <Nav.Link eventKey="disabled" disabled>
-      Disabled
-    </Nav.Link>
-  </Nav.Item>
-</Nav>
+<Navbar bg="light" expand="lg">
+  <Container fluid>
+    <Navbar.Brand href="#">Navbar scroll</Navbar.Brand>
+    <Navbar.Toggle aria-controls="navbarScroll" />
+    <Navbar.Collapse id="navbarScroll">
+      <Nav
+        className="me-auto my-2 my-lg-0"
+        style={{ maxHeight: '100px' }}
+        navbarScroll
+      >
+        <Nav.Link href="#action1">Home</Nav.Link>
+        <Nav.Link href="#action2">Link</Nav.Link>
+        <NavDropdown title="Link" id="navbarScrollingDropdown">
+          <NavDropdown.Item href="#action3">Action</NavDropdown.Item>
+          <NavDropdown.Item href="#action4">Another action</NavDropdown.Item>
+          <NavDropdown.Divider />
+          <NavDropdown.Item href="#action5">
+            Something else here
+          </NavDropdown.Item>
+        </NavDropdown>
+        <Nav.Link href="#" disabled>
+          Link
+        </Nav.Link>
+      </Nav>
+      <Form className="d-flex">
+        <SearchForm method={inputCoin}
+        />
+        <Button variant="outline-success">Search</Button>
+      </Form>
+    </Navbar.Collapse>
+  </Container>
+</Navbar>
 
-<CoinList/>
+<CoinList searchvalue={searchformvalue}/>
 </>
 
   );
--- frontend/src/Components/CoinList.js
+++ frontend/src/Components/CoinList.js
@@ -5,22 +5,42 @@ import {CountryCurrencySelector} from './CountryCurrencySelector'
 
 
 
-export const CoinList=()=>{
+export const CoinList=(props)=>{
   const[coins,setCoins]=useState(null);
   const[loading, setLoading]=useState(false);
   const[error,setError]=useState(null);
+  const[arr,setArr]=useState([]);
+
+
+ // props.searchvalue;
+  //setData(coins.filter(data => data.name ==  props.searchvalue));
+  //if search value matches data
+
+ // const arr=[];//create array to pass fixed rates
 
   useEffect(()=>{
+
+
 
 
     setLoading(true);
     fetch(`https://api.coinlore.net/api/tickers/`, {
         "method": "GET",
         headers: {
-          'Content-Type': 'application/json',
+      
            'Content-Type': 'application/x-www-form-urlencoded',
         }
-    }).then(response=>response.json()).then((c)=>setCoins(c.data)).then(()=>{setLoading(false)}).then(console.log(coins)).catch(setError);
+    }).then(response=>response.json()).then((c)=>{setCoins(c.data)
+        for (let key in c.data) {
+            if (c.data.hasOwnProperty(key)) {
+                arr.push(c.data[key].price_usd) 
+           }
+          }
+    
+          console.log(arr[9])
+          setArr(arr)
+    
+    }).then(()=>{setLoading(false)}).catch(setError);
     
    
 
@@ -34,8 +54,6 @@ return(
 
     <>
 
-<CountryCurrencySelector/>
-
     <Table striped bordered hover variant="dark">
 
 
@@ -45,30 +63,40 @@ return(
      
       <th>Coin</th>
       <th>Rank</th>
-      <th>Price in USD</th>
+      <th>
+<CountryCurrencySelector coinListPrice={coins}  resetview={setCoins} original={arr}/>
+</th>
       <th>Price in BTC</th>
       <th>Percentage Change 7 days</th>
     </tr>
   </thead>
   <tbody>
-    
-
-    {coins.map((data,key)=>(
+    {props.searchvalue===''?coins.map((data,key)=>(
 
 <tr key={key}>
       <td>{data.name}</td>
       <td>{data.rank}</td>
       <td>{data.price_usd}</td>
       <td>{data.price_btc}</td>
-      <td>{data.percent_change_7d}</td>
-
-      
-      
+      <td>{data.percent_change_7d}</td>  
       </tr>
   
 
             
-))}
+)):coins.map((data,key)=>(
+//  coins.filter(data => data.name ==  props.searchvalue)
+     (data.name.includes(props.searchvalue)?
+  <tr key={key}>
+        <td>{data.name}</td>
+        <td>{data.rank}</td>
+        <td>{data.price_usd}</td>
+        <td>{data.price_btc}</td>
+        <td>{data.percent_change_7d}</td>
+        </tr>:""
+     )
+  
+              
+  ))}
      
   </tbody>
 </Table>
